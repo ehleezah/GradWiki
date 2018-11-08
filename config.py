e80c222c1373edac4ae55184e3d74be7eb4a5d1c
Apekshya <apurimal@gmail.com>
2018-11-08 10:49:13 -0500

    Initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+SECRET_KEY='a unique and long key'
+TITLE='Riki' 
+HISTORY_SHOW_MAX=30
+PIC_BASE = '/static/content/'
+CONTENT_DIR = 'C:/Users/Apekshya/Desktop/Semester-4/Software Engineering/Project/wiki_flask/content'
+USER_DIR = 'C:/Users/Apekshya/Desktop/Semester-4/Software Engineering/Project/wiki_flask/user'
+NUMBER_OF_HISTORY = 5
+PRIVATE = True
